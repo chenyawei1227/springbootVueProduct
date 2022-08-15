f3dfefd396482f5c3c265c3026830ff8ea5e6465
yawei.chen <yawei.chen@hex-tech.net>
2022-08-15 17:34:11 +0800

    first commit

---
+++ src/main/java/com/example/springbootvueproduct/SpringbootVueProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootvueproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueProductApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootVueProductApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springbootvueproduct/SpringbootVueProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootvueproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVueProductApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
